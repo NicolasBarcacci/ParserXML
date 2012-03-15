8956f5cf5c21c37b99e01ddd2189df74e1ffabbe
Aetyno <nicolas.barcacci@gmail.com>
2012-03-15 14:17:57 +0100

    Premier commit

---
+++ src/main/java/dcll/absv/ParserXML/App.java
@@ -0,0 +1,13 @@
+package dcll.absv.ParserXML;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
