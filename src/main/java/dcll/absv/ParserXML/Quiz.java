c1cef6fb8078b4548db65983e468cb8fff480ba5
Nicolas BARCACCI <nicolas.barcacci@gmail.com>
2012-03-23 14:41:47 +0100

    Ajout de méthodes dans quizz pour manipuler les questions. Certaine méthodes sont encore à compléter.

--- src/main/java/dcll/absv/ParserXML/Quiz.java
+++ src/main/java/dcll/absv/ParserXML/Quiz.java
@@ -1,15 +1,84 @@
 package dcll.absv.ParserXML;
 
 import java.util.ArrayList;
+import java.io.File;
 
 public class Quiz {
 	
-
+	File fichierXML;
 	ArrayList<Question> questions;		// la liste des questions
+	
+	ArrayList<IQuestion> questions1;
 	
 	public Quiz(ArrayList<Question> questions) {
 		super();
 		this.questions = questions;
 	}
 	
+	public void toXML(){
+		
+	}
+	
+	public void openFile(String _file){
+		
+	}
+	
+	public IQuestion getQuestion(int _n){
+		if(_n<questions1.size()){
+			return questions1.get(_n);
+		}
+		else{
+			return null;
+		}
+	}
+	
+	public void deleteQuestion(int _n){
+		if(_n<questions1.size()){
+			questions1.remove(_n);
+		}
+	}
+	
+	public void deleteQuestion(IQuestion _question){
+		questions1.remove(_question);
+	}
+	
+	public void addQuestionCategory(){
+		questions1.add(new QuestionCategory());		
+	}
+	
+	public void addQuestionCalculated(){
+		questions1.add(new QuestionCalculated());		
+	}
+	
+	public void addQuestionDescription(){
+		questions1.add(new QuestionDescription());		
+	}
+	
+	public void addQuestionEssay(){
+		questions1.add(new QuestionEssay());		
+	}
+	
+	public void addQuestionMatching(){
+		questions1.add(new QuestionMatching());		
+	}
+	
+	public void addQuestionCloze(){
+		questions1.add(new QuestionCloze());		
+	}
+	
+	public void addQuestionMultiChoice(){
+		questions1.add(new QuestionMultiChoice());		
+	}
+	
+	public void addQuestionNumerical(){
+		questions1.add(new QuestionNumerical());		
+	}
+	
+	public void addQuestionShortAnswer(){
+		questions1.add(new QuestionShortAnswer());		
+	}
+	
+	public void addQuestionTrueFalse(){
+		questions1.add(new QuestionTrueFalse());		
+	}
 }
